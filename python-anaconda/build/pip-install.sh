9713066b5084c54ed847f12b8147b9329569af26
wonkyungup <34180230+wonkyungup@users.noreply.github.com>
2020-06-03 17:21:24 +0900

    Update pip-install.sh

--- python-anaconda/build/pip-install.sh
+++ python-anaconda/build/pip-install.sh
@@ -2,27 +2,26 @@
 
 #PATH ADD
 source ~/.bash_profile
+pip install setuptools --upgrade &&
 
 ## Install-pytorch ##
+
 #conda create -n pytorch python=3.5 -y &&
 #conda activate pytorch &&
 #conda info -e &&
 #conda install pytorch torchvision cudatoolkit=10.2 -c pytorch &&
-#pip install setuptools --upgrade &&
 
 ## Install-tensorflow ##
 #conda create -n tensorflow python=3.5 -y &&
 #conda activate tensorflow &&
 #conda info -e &&
-#pip install setuptools --upgrade &&
-#pip install tensorflow &&
+#conda install tensorflow &&
 
 ## Install-flask
 #conda create -n flask python=3.5 -y &&
 #conda activate flask &&
 #conda info -e &&
-#pip install setuptools --upgrade &&
-#pip install Flask &&
+#conda install Flask &&
 
 ## jupyter notebook Start ##
 /root/anaconda3/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
