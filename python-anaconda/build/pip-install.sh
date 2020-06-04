c8af7ce9595428f360929fb9644f1fc67abb8e8e
wonkyungup <34180230+wonkyungup@users.noreply.github.com>
2020-06-04 11:45:06 +0900

    Update pip-install.sh

--- python-anaconda/build/pip-install.sh
+++ python-anaconda/build/pip-install.sh
@@ -1,32 +1,34 @@
 #/bin/bash/
 
-#PATH ADD
-source ~/.bash_profile
+## ADD PATH
+source ~/.bashrc
 
 ## Install-pytorch #
 #conda create -n pytorch python=3.5 -y &&
 #conda activate pytorch &&
 #conda info -e &&
-#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
-#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
-#conda install pytorch torchvision cudatoolkit=10.2 -c pytorch -y &&
+#conda install setuptools --upgrade
+#conda install pytorch torchvision cudatoolkit=10.2 -c pytorch &&
+#/root/anaconda3/envs/pytorch/bin/pip install jupyter_contrib_nbextensions --upgrade pip &&
+#/root/anaconda3/envs/pytorch/bin/jupyter-contrib-nbextension install --user &&
+#/root/anaconda3/envs/pytorch/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
+
 
 ## Install-tensorflow ##
 #conda create -n tensorflow python=3.5 -y &&
 #conda activate tensorflow &&
 #conda info -e &&
-#conda install setuptools --upgrade
-#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
-#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
 #conda install tensorflow -y &&
+#/root/anaconda3/envs/tensorflow/bin/pip install jupyter_contrib_nbextensions --upgrade pip &&
+#/root/anaconda3/envs/tensorflow/bin/jupyter-contrib-nbextension install --user &&
+#/root/anaconda3/envs/tensorflow/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
+
 
 # Install-flask
 #conda create -n flask python=3.5 -y &&
 #conda activate flask &&
 #conda info -e &&
-#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
-#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
 #conda install Flask -y &&
-
-# jupyter notebook Start ##
-/root/anaconda3/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
+#/root/anaconda3/envs/flask/bin/pip install jupyter_contrib_nbextensions --upgrade pip &&
+#/root/anaconda3/envs/flask/bin/jupyter-contrib-nbextension install --user &&
+#/root/anaconda3/envs/flask/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
