ccd2d60561981510f51fd9b3edcdbb60ce836619
wonkyungup <34180230+wonkyungup@users.noreply.github.com>
2020-06-04 10:58:59 +0900

    Update pip-install.sh

--- python-anaconda/build/pip-install.sh
+++ python-anaconda/build/pip-install.sh
@@ -1,26 +1,32 @@
 #/bin/bash/
 
 #PATH ADD
-source ~/.bash_profile 
+source ~/.bash_profile
 
-## Install-pytorch ##
-
+## Install-pytorch #
 #conda create -n pytorch python=3.5 -y &&
 #conda activate pytorch &&
 #conda info -e &&
-#conda install pytorch torchvision cudatoolkit=10.2 -c pytorch &&
+#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
+#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
+#conda install pytorch torchvision cudatoolkit=10.2 -c pytorch -y &&
 
 ## Install-tensorflow ##
 #conda create -n tensorflow python=3.5 -y &&
 #conda activate tensorflow &&
 #conda info -e &&
-#conda install tensorflow &&
+#conda install setuptools --upgrade
+#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
+#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
+#conda install tensorflow -y &&
 
-## Install-flask
+# Install-flask
 #conda create -n flask python=3.5 -y &&
 #conda activate flask &&
 #conda info -e &&
-#conda install Flask &&
+#conda install -c conda-forge jupyter_contrib_nbextensions -y &&
+#/root/anaconda3/bin/jupyter contrib nbextension install --user &&
+#conda install Flask -y &&
 
-## jupyter notebook Start ##
+# jupyter notebook Start ##
 /root/anaconda3/bin/jupyter-notebook --ip=0.0.0.0 --port=8888 --allow-root
